7e45acb952df6d9a13c5f94fa796b4e3cc0ed6c7
ThaliaOlvera <resets>
2023-09-04 13:34:15 -0500

    js menu toggle function

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 console.log("Hello");
+const hamburguerMenu = document.getElementById("icon-menu");
+const mobileMenu = document.querySelector(".menu-container");
+
+const menuClients = document.querySelector(".clients");
+const menuServices = document.querySelector(".services");
+
+const clientsList = document.querySelector(".menu-clients");
+const servicesList = document.querySelector(".menu-services");
+
+hamburguerMenu.addEventListener("click", toggleMobileMenu);
+menuClients.addEventListener("click", toggleMenuClients);
+menuServices.addEventListener("click", toggleMenuServices);
+
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle("inactive");
+  clientsList.classList.toggle("inactive");
+}
+
+function toggleMenuClients() {
+  clientsList.classList.toggle("inactive");
+}
+
+function toggleMenuServices() {
+  servicesList.classList.toggle("inactive");
+}
