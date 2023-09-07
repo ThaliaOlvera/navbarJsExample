1b3af0a2e0cf2eddcd64b19c9b26035cf35e0a3c
ThaliaOlvera <resets>
2023-09-07 11:18:19 -0500

    add click touchstart

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const hamburguerMenu = document.getElementById("icon-menu");
 const mobileMenu = document.querySelector(".menu-mobile");
 
 hamburguerMenu.addEventListener("click", toggleMobileMenu);
+hamburguerMenu.addEventListener("click touchstart", toggleMobileMenu);
 
 function toggleMobileMenu() {
   mobileMenu.classList.toggle("inactive");
